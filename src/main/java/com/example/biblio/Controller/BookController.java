bb814421decce57a9a3d462fe250555dbea33b59
Саша Отрубейников <130464271+kstatimiznakomi@users.noreply.github.com>
2023-12-12 21:21:54 +0700

    book-add-gettin-started

--- src/main/java/com/example/biblio/Controller/BookController.java
+++ src/main/java/com/example/biblio/Controller/BookController.java
@@ -1,6 +1,7 @@
 package com.example.biblio.Controller;
 
 import com.example.biblio.dto.BookDTO;
+import com.example.biblio.dto.BookParamsDTO;
 import com.example.biblio.service.BookService;
 import com.example.biblio.service.UserService;
 import lombok.AllArgsConstructor;
@@ -14,18 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.security.Principal;
 
 @Controller
-@RequestMapping("/book/{bookId}")
+@RequestMapping("/book")
 @AllArgsConstructor
 @Lazy
 public class BookController {
     private final BookService bookService;
     private final UserService userService;
 
-    @GetMapping("")
+    @GetMapping("/{bookId}")
     public String aboutBook(Model model, @PathVariable Long bookId, Principal principal){
         if(principal != null) model.addAttribute("user", userService.getUserByName(principal.getName()));
         BookDTO book = bookService.getBookPage(bookId);
         model.addAttribute("book", book);
         return "about-book";
+    }
+
+    @GetMapping("/add")
+    public String addBookPage(Model model, Principal principal){
+        if(principal != null) model.addAttribute("user", userService.getUserByName(principal.getName()));
+        model.addAttribute("book", new BookParamsDTO());
+        return "";
     }
 }
